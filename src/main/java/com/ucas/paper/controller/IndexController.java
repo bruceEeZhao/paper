3a61b8eb48eb2a70d05fdbb65da056b586f619b3
bruceEeZhao <bruce.e.zhao@gmail.com>
2020-10-19 11:04:30 +0800

    1.修复文件下载出错问题;2.修复网页加载时间长的问题：将googleapis修改为fonts.css.network

--- src/main/java/com/ucas/paper/controller/IndexController.java
+++ src/main/java/com/ucas/paper/controller/IndexController.java
@@ -12,8 +12,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -22,6 +24,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.*;
+import java.net.URLConnection;
 import java.net.URLEncoder;
 import java.util.List;
 
@@ -101,61 +104,65 @@ public class IndexController {
         Integer flag = 0;
 
         JournalPdf journalPdf = journalPdfService.getByName("国际期刊");
-        if (journalPdf==null) {
+        if (journalPdf == null) {
             return "管理员未上传,下载失败";
         }
 
 
         String fileName = journalPdf.getFilename();
         if (fileName != null) {
-            String type=fileName.substring(fileName.indexOf("."));
+            String type = fileName.substring(fileName.indexOf("."));
             //设置文件路径
             File file = new File(fileName);
             if (file.exists()) {
-                response.setContentType("application/force-download");// 设置强制下载不打开
-                response.addHeader("Content-Disposition", "attachment;fileName=" + "global"+type);// 设置文件名
-                byte[] buffer = new byte[1024];
-                FileInputStream fis = null;
-                BufferedInputStream bis = null;
-                try {
-                    fis = new FileInputStream(file);
-                    bis = new BufferedInputStream(fis);
-                    OutputStream os = response.getOutputStream();
-                    int i = bis.read(buffer);
-                    while (i != -1) {
-                        os.write(buffer, 0, i);
-                        i = bis.read(buffer);
-                    }
+                //get the mimetype
+                String mimeType = URLConnection.guessContentTypeFromName(file.getName());
+                if (mimeType == null) {
+                    //unknown mimetype so set the mimetype to application/octet-stream
+                    mimeType = "application/octet-stream";
+                }
 
-                    //attributes.addFlashAttribute("message", "下载成功");
+                response.setContentType(mimeType);
+
+                /**
+                 * In a regular HTTP response, the Content-Disposition response header is a
+                 * header indicating if the content is expected to be displayed inline in the
+                 * browser, that is, as a Web page or as part of a Web page, or as an
+                 * attachment, that is downloaded and saved locally.
+                 *
+                 */
+
+                /**
+                 * Here we have mentioned it to attachment
+                 */
+                response.setHeader("Content-Disposition", String.format("attachment; filename=\"" + "global" + type + "\""));
+
+                //Here we have mentioned it to show as attachment
+                //response.setHeader("Content-Disposition", String.format("inline; filename=\"" + "global" + type + "\""));
+
+                response.setContentLength((int) file.length());
+
+                InputStream inputStream = null;
+                try {
+                    inputStream = new BufferedInputStream(new FileInputStream(file));
+                    FileCopyUtils.copy(inputStream, response.getOutputStream());
                     flag = 1;
-                } catch (Exception e) {
-                    //attributes.addFlashAttribute("message", "下载失败");
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
                     logger.error(e.toString());
-                } finally {
-                    if (bis != null) {
-                        try {
-                            bis.close();
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                    if (fis != null) {
-                        try {
-                            fis.close();
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    logger.error(e.toString());
                 }
             }
         }
-        if (flag==1) {
+        if (flag == 1) {
             return "下载成功";
         } else {
             return "下载失败";
         }
     }
+
 
     @ResponseBody
     @GetMapping("download_cn_pdf")
@@ -170,44 +177,48 @@ public class IndexController {
 
         String fileName = journalPdf.getFilename();
         if (fileName != null) {
+            String type = fileName.substring(fileName.indexOf("."));
             //设置文件路径
             File file = new File(fileName);
             if (file.exists()) {
-                String type = fileName.substring(fileName.indexOf("."));
-                response.setContentType("application/force-download");// 设置强制下载不打开
-                response.addHeader("Content-Disposition", "attachment;fileName=" + "chinese"+type);// 设置文件名
-                byte[] buffer = new byte[1024];
-                FileInputStream fis = null;
-                BufferedInputStream bis = null;
+                //get the mimetype
+                String mimeType = URLConnection.guessContentTypeFromName(file.getName());
+                if (mimeType == null) {
+                    //unknown mimetype so set the mimetype to application/octet-stream
+                    mimeType = "application/octet-stream";
+                }
+
+                response.setContentType(mimeType);
+
+                /**
+                 * In a regular HTTP response, the Content-Disposition response header is a
+                 * header indicating if the content is expected to be displayed inline in the
+                 * browser, that is, as a Web page or as part of a Web page, or as an
+                 * attachment, that is downloaded and saved locally.
+                 *
+                 */
+
+                /**
+                 * Here we have mentioned it to attachment
+                 */
+                response.setHeader("Content-Disposition", String.format("attachment; filename=\"" + "chinese" + type + "\""));
+
+                //Here we have mentioned it to show as attachment
+                //response.setHeader("Content-Disposition", String.format("inline; filename=\"" + "global" + type +  "\""));
+
+                response.setContentLength((int) file.length());
+
+                InputStream inputStream = null;
                 try {
-                    fis = new FileInputStream(file);
-                    bis = new BufferedInputStream(fis);
-                    OutputStream os = response.getOutputStream();
-                    int i = bis.read(buffer);
-                    while (i != -1) {
-                        os.write(buffer, 0, i);
-                        i = bis.read(buffer);
-                    }
-
+                    inputStream = new BufferedInputStream(new FileInputStream(file));
+                    FileCopyUtils.copy(inputStream, response.getOutputStream());
                     flag = 1;
-
-                } catch (Exception e) {
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
                     logger.error(e.toString());
-                } finally {
-                    if (bis != null) {
-                        try {
-                            bis.close();
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                    if (fis != null) {
-                        try {
-                            fis.close();
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    logger.error(e.toString());
                 }
             }
         }
